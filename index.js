f7f9b2baf507c9e8e0ead00ce27bed9bc3c3067b
9400749917megha <meghasaju45@gmail.com>
2024-09-09 14:13:46 +0530

    deploy

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: 'https://carista-rental.vercel.app',
     credentials:true,
     allowedHeaders: ['Content-Type', 'Authorization']
 }));
