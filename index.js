a281e0822534fe9637ffcb1615c3ed83078e448d
9400749917megha <meghasaju45@gmail.com>
2024-09-16 16:16:38 +0530

    cors error fix

--- index.js
+++ index.js
@@ -14,7 +14,7 @@ connectDB();
 
 // Middleware
 app.use(cors({
-    origin: 'http://localhost:5173',
+    origin: ['http://localhost:5173','https://carista-rental.vercel.app'],
     
     credentials:true,
 }));
